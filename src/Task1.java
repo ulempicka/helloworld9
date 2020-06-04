a5df66fc537d117cb1acdf47835491302df9234a
ulempicka <urszula.lempicka@gmail.com>
2020-06-04 16:51:35 +0200

    commit1

---
+++ src/Task1.java
@@ -0,0 +1,10 @@
+public class Task1 {
+    public static void main (String [] args){
+        System.out.println("Uczę");
+        System.out.println("się");
+        System.out.println("programowania");
+        System.out.println("i");
+        System.out.println("commitowania");
+        System.out.print("i\nwiecej\ncommitowania");
+    }
+}
